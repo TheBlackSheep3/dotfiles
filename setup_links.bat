18ecaee7fe998a17a3e9bd998f9ad3e8bebe2605
Diessner, Niklas <Niklas.Diessner@bruker.com>
2023-10-17 09:25:52 +0200

    create lazygit junction

--- setup_links.bat
+++ setup_links.bat
@@ -1,3 +1,4 @@
 mklink /j %LOCALAPPDATA%\nvim %HOME%\.dotfiles\neovim
 mklink /j %APPDATA%\gitui %HOME%\.dotfiles\gitui
 mklink /j %APPDATA%\nushell %HOME%\.dotfile\nushell
+mklink /j %APPDATA%\lazygit %HOME%\.dotfile\lazygit
