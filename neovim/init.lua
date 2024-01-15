f4c9271ad5342d8880fc8f5b417e69690c17cff3
Diessner, Niklas <Niklas.Diessner@bruker.com>
2024-01-15 13:34:05 +0100

    replace checkbox icons

--- neovim/init.lua
+++ neovim/init.lua
@@ -726,8 +726,8 @@ require("obsidian").setup({
       -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
       [" "] = { char = "", hl_group = "ObsidianTodo" },
       ["x"] = { char = "", hl_group = "ObsidianDone" },
-      [">"] = { char = ">", hl_group = "ObsidianRightArrow" },
-      ["~"] = { char = " ", hl_group = "ObsidianTilde" },
+      [">"] = { char = "󰧚", hl_group = "ObsidianRightArrow" },
+      ["~"] = { char = "", hl_group = "ObsidianTilde" },
       -- Replace the above with this if you don't have a patched font:
       -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
       -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
